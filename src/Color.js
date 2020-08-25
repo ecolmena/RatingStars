4f72f6d7394ea2c8ebb2bceb4ece681efecf4c13
chrlnmrtn <novillacharlene@gmail.com>
2020-08-24 18:10:35 -0600

    StarRating

---
+++ src/Color.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import StarRating from "./StarRating";
+
+export default function Color() {
+  return (
+    <section>
+      <h1>Color Organizer</h1>
+      <div />
+      <StarRating />
+    </section>
+  );
+}
